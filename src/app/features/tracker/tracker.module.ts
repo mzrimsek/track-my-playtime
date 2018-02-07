61366e9e0415bf05b2a8be27f40f06dfc2cb004f
Mike Zrimsek <mikezrimsek@gmail.com>
2018-02-07 15:34:41 -0500

    add timer and history components

--- src/app/features/tracker/tracker.module.ts
+++ src/app/features/tracker/tracker.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TrackerComponent } from './tracker.component';
+import { TimerComponent } from './components/timer/timer.component';
+import { HistoryComponent } from './components/history/history.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [TrackerComponent]
+  declarations: [TrackerComponent, TimerComponent, HistoryComponent]
 })
 export class TrackerModule { }
