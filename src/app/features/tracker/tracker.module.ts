c20312ccefad07d205608900937f4638551e4096
Mike Zrimsek <mikezrimsek@gmail.com>
2018-02-08 01:09:53 -0500

    add reducers to tracker module

--- src/app/features/tracker/tracker.module.ts
+++ src/app/features/tracker/tracker.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { StoreModule } from '@ngrx/store';
+
 import { TrackerComponent } from './tracker.component';
 import { TimerComponent } from './components/timer/timer.component';
 import { HistoryComponent } from './components/history/history.component';
 
+import { reducers } from './reducers';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forFeature('tracker', reducers)
   ],
   declarations: [TrackerComponent, TimerComponent, HistoryComponent]
 })
