0204b7dcd2de54892913ad36071b1b16f8b77aec
Mike Zrimsek <mikezrimsek@gmail.com>
2018-02-21 14:25:05 -0500

    broke history items into separate component
    history component is almost purely compositional and for passing data

--- src/app/features/tracker/components/history-entry/history-entry.component.ts
+++ src/app/features/tracker/components/history-entry/history-entry.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { HistoryListItem } from '../../models';
+import { State } from '../../reducers';
+import * as actions from '../../actions/history';
 
 @Component({
-  selector: 'app-history-entry',
+  selector: 'app-tracker-history-entry',
   templateUrl: './history-entry.component.html',
-  styleUrls: ['./history-entry.component.scss']
+  styleUrls: ['./history-entry.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HistoryEntryComponent implements OnInit {
 
-  constructor() { }
+  @Input() item: HistoryListItem;
+  @Input() platforms: string[];
+  constructor(private store: Store<State>) { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+  updateGame(gameEl: HTMLInputElement) {
+    if (gameEl.value) {
+      this.store.dispatch(new actions.UpdateGame(this.item.id, gameEl.value));
+    }
   }
 
+  updatePlatform(platformEl: HTMLSelectElement) {
+    if (platformEl.value) {
+      this.store.dispatch(new actions.UpdatePlatform(this.item.id, platformEl.value));
+    }
+  }
+
+  remove() {
+    this.store.dispatch(new actions.RemoveHistoryItem(this.item.id));
+  }
 }
--- src/app/features/tracker/components/history/history.component.ts
+++ src/app/features/tracker/components/history/history.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, Input, ChangeDetectionStrategy } from '@angular/core';
-import { Store } from '@ngrx/store';
 import { HistoryListItem } from '../../models';
-import { State } from '../../reducers';
-import * as actions from '../../actions/history';
 
 @Component({
   selector: 'app-tracker-history',
@@ -14,23 +11,7 @@ export class HistoryComponent implements OnInit {
 
   @Input() items: HistoryListItem[];
   @Input() platforms: string[];
-  constructor(private store: Store<State>) { }
+  constructor() { }
 
   ngOnInit() { }
-
-  updateGame(id: string, gameEl: HTMLInputElement) {
-    if (gameEl.value) {
-      this.store.dispatch(new actions.UpdateGame(id, gameEl.value));
-    }
-  }
-
-  updatePlatform(id: string, platformEl: HTMLSelectElement) {
-    if (platformEl.value) {
-      this.store.dispatch(new actions.UpdatePlatform(id, platformEl.value));
-    }
-  }
-
-  remove(id: string) {
-    this.store.dispatch(new actions.RemoveHistoryItem(id));
-  }
 }
