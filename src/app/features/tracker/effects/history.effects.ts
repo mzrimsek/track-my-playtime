3c4444b39cb03273d35200ed3ee51e38a508b3f1
Mike Zrimsek <mikezrimsek@gmail.com>
2018-02-21 11:53:01 -0500

    reorganized effects

--- src/app/features/tracker/effects/history.effects.ts
+++ src/app/features/tracker/effects/history.effects.ts
@@ -1,14 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
+import { HistoryService } from '../services/history.service';
 import * as timerActions from '../actions/timer';
 import * as historyActions from '../actions/history';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 
 @Injectable()
 export class HistoryEffects {
 
-  constructor(private actions$: Actions) { }
+  constructor(private actions$: Actions, private historyService: HistoryService) { }
+
+  @Effect() loadHistoryItems$ =
+    this.actions$
+      .ofType(historyActions.LOAD_HISTORY_ITEMS)
+      .switchMap(() => this.historyService.getHistoryList()
+        .map(data => {
+          return new historyActions.LoadHistoryItemsSucceeded(data);
+        }));
 
   @Effect() saveTimerInfoSucceeded$ =
     this.actions$
--- src/app/features/tracker/effects/initialization.effects.ts
+++ src/app/features/tracker/effects/initialization.effects.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect } from '@ngrx/effects';
-import { TimerService } from '../services/timer.service';
-import { HistoryService } from '../services/history.service';
 import * as appActions from '../../../actions';
 import * as timerActions from '../actions/timer';
 import * as historyActions from '../actions/history';
@@ -12,7 +10,7 @@ import 'rxjs/add/operator/mergeMap';
 @Injectable()
 export class TrackerInitializationEffects {
 
-  constructor(private actions$: Actions, private timerService: TimerService, private historyService: HistoryService) { }
+  constructor(private actions$: Actions) { }
 
   @Effect() initialize$ =
     this.actions$
@@ -22,19 +20,7 @@ export class TrackerInitializationEffects {
         new historyActions.LoadHistoryItems()
       ]);
 
-  @Effect() loadPlatforms$ =
-    this.actions$
-      .ofType(timerActions.LOAD_PLATFORMS)
-      .switchMap(() => this.timerService.getPlatforms()
-        .map(data => {
-          return new timerActions.LoadPlatformsSucceeded(data);
-        }));
 
-  @Effect() loadHistoryItems$ =
-    this.actions$
-      .ofType(historyActions.LOAD_HISTORY_ITEMS)
-      .switchMap(() => this.historyService.getHistoryList()
-        .map(data => {
-          return new historyActions.LoadHistoryItemsSucceeded(data);
-        }));
+
+
 }
