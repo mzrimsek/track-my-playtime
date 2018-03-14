c333ad033084793a0b452a38e14aab4469b68002
Mike Zrimsek <mikezrimsek@gmail.com>
2018-03-13 22:57:49 -0400

    basic home to login component workflow added

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
+import { AppRoutingModule } from '../../app-routing.module';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    AppRoutingModule
   ],
   declarations: [HomeComponent]
 })
