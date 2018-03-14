ef23a44683afc00e554f28dd2592ada30a282c48
Mike Zrimsek <mikezrimsek@gmail.com>
2018-03-14 00:18:33 -0400

    add tracker module

---
+++ src/app/features/tracker-app/tracker-app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TrackerAppComponent } from './tracker-app.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [TrackerAppComponent]
+})
+export class TrackerAppModule { }
