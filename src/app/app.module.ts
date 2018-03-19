fcd2f2d905cb1216ed76f2c31cf4aa5edd8309ce
Mike Zrimsek <mikezrimsek@gmail.com>
2018-03-19 12:24:35 -0400

    removed imports - having it all in one place removes errors and I keep forgetting where to look to make changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,37 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 import { AppComponent } from './app.component';
 
-import imports from './imports';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+
+import { TrackerModule } from './features/tracker/tracker.module';
+import { HomeModule } from './features/home/home.module';
+
+import { reducers } from './reducers/root.reducer';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports,
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    SharedModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+    TrackerModule,
+    HomeModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
