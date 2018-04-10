0f3263f5a2a5e73a0ada7c3ba243b3cd568b6072
Mike Zrimsek <mikezrimsek@gmail.com>
2018-04-10 11:29:23 -0400

    Displaying the time vs date graph
    This will later move to it's own component and the inputs will be refactored to be more reusable...this is just first steps

--- src/app/features/dashboard/dashboard.component.ts
+++ src/app/features/dashboard/dashboard.component.ts
@@ -6,12 +6,29 @@ import { GraphService } from './services/graph.service';
 
 import { BarGraphDataItem } from './models';
 
+import { formatElapsedTime } from '../../shared/utils/date.utils';
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+
+  view: any[] = [700, 400];
+  showXAxis = true;
+  showYAxis = true;
+  gradient = false;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Date';
+  showYAxisLabel = true;
+  yAxisLabel = 'Total Time Played';
+  yAxisTickFormatting = formatElapsedTime;
+
+  colorScheme = {
+    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+  };
 
   timeVsDateGraphData$: Observable<BarGraphDataItem[]>;
   constructor(private graphService: GraphService) { }
