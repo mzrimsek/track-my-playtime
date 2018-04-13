22dde076095aa6ddf42092ac0297dce73be111ca
Mike Zrimsek <mikezrimsek@gmail.com>
2018-04-12 21:48:49 -0400

    All utils are tested!

--- src/app/features/dashboard/utils/colorScheme.utils.ts
+++ src/app/features/dashboard/utils/colorScheme.utils.ts
@@ -3,9 +3,11 @@ import { colorSets } from '@swimlane/ngx-charts/release/utils/color-sets';
 type ColorSetName = 'vivid' | 'natural' | 'cool' | 'fire' |
   'solar' | 'air' | 'aqua' | 'flame' |
   'ocean' | 'forest' | 'horizon' | 'neons' |
-  'picnic' | 'night' | 'nightLights';
+  'picnic' | 'night' | 'nightLights' | 'default';
+
+export const DEFAULT_COLOR_SCHEME = ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'];
 
 export const selectColorScheme = (name: ColorSetName): string[] => {
   const selectedSet = colorSets.find(set => set.name === name);
-  return selectedSet ? selectedSet.domain : ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA'];
+  return selectedSet ? selectedSet.domain : DEFAULT_COLOR_SCHEME;
 };
