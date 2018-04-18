b0c0b41188eaba56dc7b48054e4e8d721ec4fed1
Mike Zrimsek <mikezrimsek@gmail.com>
2018-04-18 14:54:02 -0400

    add actions tests

---
+++ src/app/features/tracker/actions/platforms.actions.spec.ts
@@ -0,0 +1,28 @@
+import * as actions from './platforms.actions';
+
+describe('Platforms Actions', () => {
+  describe('LoadOptions', () => {
+    it('Should have correct type', () => {
+      const action = new actions.LoadOptions();
+      expect(action.type).toBe(actions.LOAD_OPTIONS);
+    });
+  });
+
+  describe('LoadOptionsSucceeded', () => {
+    it('Should have correct type', () => {
+      const action = new actions.LoadOptionsSucceeded([]);
+      expect(action.type).toBe(actions.LOAD_OPTIONS_SUCCEEDED);
+    });
+
+    it('Should have correct platforms', () => {
+      const platforms = [
+        'Game Box 720',
+        'Nipkendo Scratch',
+        'Dudestation 69'
+      ];
+      const action = new actions.LoadOptionsSucceeded(platforms);
+
+      expect(action.platforms).toEqual(platforms);
+    });
+  });
+});
