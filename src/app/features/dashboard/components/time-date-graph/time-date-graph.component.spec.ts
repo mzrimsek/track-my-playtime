f745b2f214a443b2bf93771f1daa7ee07f684343
Mike Zrimsek <mikezrimsek@gmail.com>
2018-04-18 12:11:03 -0400

    add graph component tests not really sure what to test here since it's just a shallow wrapper over ngx-charts components

---
+++ src/app/features/dashboard/components/time-date-graph/time-date-graph.component.spec.ts
@@ -0,0 +1,24 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TimeDateGraphComponent } from './time-date-graph.component';
+
+describe('TimeDateGraphComponent', () => {
+  let component: TimeDateGraphComponent;
+  let fixture: ComponentFixture<TimeDateGraphComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        TimeDateGraphComponent
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
+    fixture = TestBed.createComponent(TimeDateGraphComponent);
+    component = fixture.componentInstance;
+  }));
+
+  it('Should create the component', async(() => {
+    expect(component).toBeTruthy();
+  }));
+});
