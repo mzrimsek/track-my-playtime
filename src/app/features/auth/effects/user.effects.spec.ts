458efd12d59970f319763d5b6d5441d9ffebb083
Mike Zrimsek <mikezrimsek@gmail.com>
2018-04-22 16:14:46 -0400

    not sure how to correctly split log out test into two tests

--- src/app/features/auth/effects/user.effects.spec.ts
+++ src/app/features/auth/effects/user.effects.spec.ts
@@ -1,18 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import { ActivatedRoute, Router } from '@angular/router';
 
 import { provideMockActions } from '@ngrx/effects/testing';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import { cold, hot } from 'jasmine-marbles';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 import { UserEffects } from './user.effects';
+
+import * as userActions from '../actions/user.actions';
 
 describe('User Effects', () => {
   let actions: any;
   let effects: UserEffects;
+  const router = {
+    navigate: jasmine.createSpy('navigate')
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [
         UserEffects,
-        provideMockActions(() => actions)
+        provideMockActions(() => actions),
+        { provide: AngularFireAuth, useValue: angularFireAuthStub },
+        { provide: Router, useValue: router },
+        { provide: ActivatedRoute, useValue: mockActivatedRoute }
       ]
     });
     effects = TestBed.get(UserEffects);
@@ -20,31 +33,53 @@ describe('User Effects', () => {
 
   describe('Get User', () => {
     it('Should dispatch Authenticated when user is authenticated', () => {
-
+      fail();
     });
 
     it('Should navigate to return url when user is authenticated', () => {
-
+      fail();
     });
 
     it('Should dispatch NotAuthenticated when user is not authenticated', () => {
-
+      fail();
     });
   });
 
   describe('Google Login', () => {
     it('Should dispatch GetUser', () => {
-
+      fail();
     });
   });
 
   describe('Logout', () => {
-    it('Should dispatch NotAuthenticated', () => {
-
-    });
-
-    it('Should navgiate to login', () => {
-
+    it('Should dispatch NotAuthenticated and navigate to login', () => {
+      actions = hot('-a', { a: new userActions.Logout() });
+      const expected = cold('-(b)', {
+        b: new userActions.NotAuthenticated()
+      });
+      expect(effects.logout$).toBeObservable(expected);
+      expect(router.navigate).toHaveBeenCalledWith(['login']);
     });
   });
 });
+
+const fakeAuthState = new BehaviorSubject(null);
+
+const fakeSignOutHandler = (): Promise<any> => {
+  fakeAuthState.next(null);
+  return Promise.resolve();
+};
+
+const angularFireAuthStub = {
+  authState: fakeAuthState,
+  auth: {
+    signOut: jasmine
+      .createSpy('signOut')
+      .and
+      .callFake(fakeSignOutHandler),
+  },
+};
+
+const mockActivatedRoute = {
+  queryParams: {}
+};
