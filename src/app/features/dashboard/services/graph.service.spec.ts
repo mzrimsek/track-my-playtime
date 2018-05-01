cb1410ca262254add3c512acbf64ec4d05d7dc10
Mike Zrimsek <mikezrimsek@gmail.com>
2018-05-01 09:37:52 -0400

    finish graph service tests

--- src/app/features/dashboard/services/graph.service.spec.ts
+++ src/app/features/dashboard/services/graph.service.spec.ts
@@ -2,12 +2,17 @@ import { TestBed } from '@angular/core/testing';
 
 import { combineReducers, Store, StoreModule } from '@ngrx/store';
 
+import { addDays, startOfWeek, subDays } from 'date-fns';
+
 import { GraphService } from './graph.service';
 
 import * as historyActions from '../../tracker/actions/history.actions';
 
 import * as fromRoot from '../../../reducers/root.reducer';
+import { HistoryEntity } from '../../tracker/reducers/history.reducer';
 import * as fromTracker from '../../tracker/reducers/root.reducer';
+
+import { formatDate } from '../../../shared/utils/date.utils';
 
 describe('Graph Service', () => {
   let service: GraphService;
@@ -53,7 +58,34 @@ describe('Graph Service', () => {
     });
 
     describe('When data in range', () => {
-
+      it('Should return the correct data', () => {
+        store.dispatch(new historyActions.LoadHistoryItemsSucceeded(testItems));
+        const result = service.getTimeVsDateGraphData();
+        result.subscribe(res => {
+          expect(res).toEqual([{
+            name: formatDate(rangeStart),
+            value: 1
+          }, {
+            name: formatDate(addDays(rangeStart, 1)),
+            value: 0
+          }, {
+            name: formatDate(addDays(rangeStart, 2)),
+            value: 0
+          }, {
+            name: formatDate(addDays(rangeStart, 3)),
+            value: 0
+          }, {
+            name: formatDate(addDays(rangeStart, 4)),
+            value: 4
+          }, {
+            name: formatDate(addDays(rangeStart, 5)),
+            value: 0
+          }, {
+            name: formatDate(addDays(rangeStart, 6)),
+            value: 0
+          }]);
+        });
+      });
     });
   });
 
@@ -64,6 +96,20 @@ describe('Graph Service', () => {
         expect(res.length).toBe(0);
       });
     });
+
+    it('Should return the correct data when in range', () => {
+      store.dispatch(new historyActions.LoadHistoryItemsSucceeded(testItems));
+      const result = service.getTimeVsPlatformGraphData();
+      result.subscribe(res => {
+        expect(res).toEqual([{
+          name: 'platform 1',
+          value: 4
+        }, {
+          name: 'platform 2',
+          value: 1
+        }]);
+      });
+    });
   });
 
   describe('getTimeVsGameGraphData', () => {
@@ -71,6 +117,20 @@ describe('Graph Service', () => {
       const result = service.getTimeVsGameGraphData();
       result.subscribe(res => {
         expect(res.length).toBe(0);
+      });
+    });
+
+    it('Should return the correct data when in range', () => {
+      store.dispatch(new historyActions.LoadHistoryItemsSucceeded(testItems));
+      const result = service.getTimeVsGameGraphData();
+      result.subscribe(res => {
+        expect(res).toEqual([{
+          name: 'game 2',
+          value: 4
+        }, {
+          name: 'game 1',
+          value: 1
+        }]);
       });
     });
   });
@@ -92,3 +152,31 @@ describe('Graph Service', () => {
     });
   });
 });
+
+const rangeStart = startOfWeek(new Date());
+
+const testItems: HistoryEntity[] = [{
+  id: '1',
+  game: 'game 1',
+  platform: 'platform 1',
+  startTime: rangeStart.getTime(),
+  endTime: rangeStart.getTime() + 1000
+}, {
+  id: '2',
+  game: 'game 2',
+  platform: 'platform 1',
+  startTime: addDays(rangeStart, 4).getTime(),
+  endTime: addDays(rangeStart, 4).getTime() + 3000,
+}, {
+  id: '3',
+  game: 'game 1',
+  platform: 'platform 2',
+  startTime: subDays(rangeStart, 1).getTime(),
+  endTime: subDays(rangeStart, 1).getTime() + 2000
+}, {
+  id: '4',
+  game: 'game 2',
+  platform: 'platform 2',
+  startTime: addDays(rangeStart, 4).getTime(),
+  endTime: addDays(rangeStart, 4).getTime() + 1000
+}];
