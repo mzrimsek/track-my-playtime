4e9a232dfdb335e0cd168e10f098e7659e905538
Mike Zrimsek <mikezrimsek@gmail.com>
2018-05-04 11:18:42 -0400

    finish tests for history component

--- src/app/features/tracker/components/history/history.component.spec.ts
+++ src/app/features/tracker/components/history/history.component.spec.ts
@@ -1,24 +1,27 @@
-import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { Component, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HistoryComponent } from './history.component';
 
 import { TimePipe } from '../../../../shared/pipes/time.pipe';
 
+import { HistoryGrouping } from '../../models';
+
 describe('HistoryComponent', () => {
-  let component: HistoryComponent;
-  let fixture: ComponentFixture<HistoryComponent>;
+  let component: TestWrapperComponent;
+  let fixture: ComponentFixture<TestWrapperComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
+        TestWrapperComponent,
         HistoryComponent,
         TimePipe,
       ],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HistoryComponent);
+    fixture = TestBed.createComponent(TestWrapperComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -33,8 +36,7 @@ describe('HistoryComponent', () => {
       fixture.detectChanges();
     }));
 
-    // FIXME: Not sure why setting component to loading isn't working
-    xit('Should show loading spinner', async(() => {
+    it('Should show loading spinner', async(() => {
       const spinner = fixture.nativeElement.querySelector('app-shared-spinner');
       expect(spinner).toBeTruthy();
     }));
@@ -56,47 +58,81 @@ describe('HistoryComponent', () => {
         const noGroups = fixture.nativeElement.querySelector('.no-groups');
         expect(noGroups.innerText).toBe('Nothing to show...get tracking!');
       });
+
+      it('Should not show any groups', () => {
+        const groups = fixture.nativeElement.querySelector('.groups');
+        expect(groups).toBeNull();
+      });
     });
 
     describe('When groups has data', () => {
+      let groups: any;
+
       beforeEach(async(() => {
         component.groups = [{
-          key: '5/3/2018',
-          historyItems: [{
-            id: '1',
-            game: 'some game',
-            platform: 'some platform',
-            startTime: 3000,
-            endTime: 6000,
-            dateRange: [new Date('5/3/2018'), new Date('5/3/2018')]
-          }],
-          totalTime: 3000
-        }, {
           key: '5/2/2018',
           historyItems: [{
-            id: '2',
+            id: '1',
             game: 'some game 2',
             platform: 'some platform',
-            startTime: 30000,
-            endTime: 60000,
+            startTime: 7000,
+            endTime: 10000,
             dateRange: [new Date('5/2/2018'), new Date('5/2/2018')]
           },
           {
-            id: '3',
+            id: '2',
             game: 'some game 2',
             platform: 'some platform 2',
             startTime: 3000,
             endTime: 6000,
             dateRange: [new Date('5/2/2018'), new Date('5/2/2018')]
           }],
-          totalTime: 33000
+          totalTime: 6
         }];
         fixture.detectChanges();
+        groups = fixture.nativeElement.querySelector('.groups');
       }));
 
       it('Should show the correct number of groups', () => {
+        expect(groups.children.length).toBe(1);
+      });
 
+      describe('Group information', () => {
+        let group: any;
+
+        beforeEach(async(() => {
+          group = groups.children[0];
+        }));
+
+        it('Should have correct date', async(() => {
+          const date = group.querySelector('.date');
+          expect(date.innerText).toBe('5/2/2018');
+        }));
+
+        it('Should have correct elasped time', async(() => {
+          const elapsedTime = group.querySelector('.elapsedTime');
+          expect(elapsedTime.innerText).toBe('00:00:06');
+        }));
+
+        it('Should have correct number of history entries', () => {
+          const historyEntries = group.querySelector('.history-entries');
+          expect(historyEntries.children.length).toBe(2);
+        });
       });
     });
   });
 });
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'test-wrapper',
+  template: `
+    <app-tracker-history [loading]="loading" [groups]="groups"></app-tracker-history>
+  `
+})
+class TestWrapperComponent implements OnInit {
+  loading = false;
+  groups: HistoryGrouping[] = [];
+
+  ngOnInit() { }
+}
