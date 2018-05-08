9d8440c26889a19a903161e9b11d5acc9cfe44c6
Mike Zrimsek <mikezrimsek@gmail.com>
2018-05-08 10:33:27 -0400

    added a couple hardcoded date ranges

--- src/app/features/dashboard/actions/date-range.actions.ts
+++ src/app/features/dashboard/actions/date-range.actions.ts
@@ -12,5 +12,26 @@ export class SetEndDay implements Action {
   constructor(public end: string) { }
 }
 
+export const SET_THIS_WEEK = '[Dashboard] Set This Week';
+export class SetThisWeek implements Action {
+  readonly type = SET_THIS_WEEK;
+  constructor() { }
+}
+
+export const SET_LAST_WEEK = '[Dashboard] Set Last Week';
+export class SetLastWeek implements Action {
+  readonly type = SET_LAST_WEEK;
+  constructor() { }
+}
+
+export const SET_THIS_MONTH = '[Dashboard] Set This Month';
+export class SetThisMonth implements Action {
+  readonly type = SET_THIS_MONTH;
+  constructor() { }
+}
+
 export type All = SetStartDay |
-  SetEndDay;
+  SetEndDay |
+  SetThisWeek |
+  SetLastWeek |
+  SetThisMonth;
--- src/app/features/dashboard/reducers/date-range.reducer.ts
+++ src/app/features/dashboard/reducers/date-range.reducer.ts
@@ -1,4 +1,4 @@
-import { addDays, startOfWeek } from 'date-fns';
+import { endOfMonth, endOfWeek, startOfMonth, startOfWeek, subDays } from 'date-fns';
 import { tassign } from 'tassign';
 
 import * as actions from '../actions/date-range.actions';
@@ -8,14 +8,13 @@ export interface State {
   endDay: Date;
 }
 
-const start = startOfWeek(new Date());
-const end = addDays(start, 6);
-const initalState: State = {
-  startDay: start,
-  endDay: end
+const now = new Date();
+const initialState: State = {
+  startDay: startOfWeek(now),
+  endDay: endOfWeek(now)
 };
 
-export function reducer(state: State = initalState, action: actions.All): State {
+export function reducer(state: State = initialState, action: actions.All): State {
   switch (action.type) {
     case actions.SET_START_DAY: {
       const startDay = new Date(action.start);
@@ -24,6 +23,26 @@ export function reducer(state: State = initalState, action: actions.All): State 
     case actions.SET_END_DAY: {
       const endDay = new Date(action.end);
       return tassign(state, { endDay });
+    }
+    case actions.SET_THIS_WEEK: {
+      return initialState;
+    }
+    case actions.SET_LAST_WEEK: {
+      const startThisWeek = startOfWeek(now);
+      const startLastWeek = subDays(startThisWeek, 7);
+      const endLastWeek = endOfWeek(startLastWeek);
+      return tassign(state, {
+        startDay: startLastWeek,
+        endDay: endLastWeek
+      });
+    }
+    case actions.SET_THIS_MONTH: {
+      const startMonth = startOfMonth(now);
+      const endMonth = endOfMonth(now);
+      return tassign(state, {
+        startDay: startMonth,
+        endDay: endMonth
+      });
     }
     default: {
       return state;
