0a5f136fa80782d193b3d3d17103bdbbf53b98ac
Mike Zrimsek <mikezrimsek@gmail.com>
2018-05-09 12:58:56 -0400

    add ga tag

--- src/app/features/dashboard/services/graph.service.ts
+++ src/app/features/dashboard/services/graph.service.ts
@@ -15,11 +15,10 @@ import { filterGroupingsByDateRange, mapToGraphData, padDateGraphData } from '..
 export class GraphService {
 
   private dateRange: Date[] = [];
-  constructor(private trackerStore: Store<TrackerState>, private dashboardStore: Store<DashboardState>) {
-    this.dashboardStore.select(dashboardSelectors.dateList).subscribe(dateList => this.dateRange = dateList);
-  }
+  constructor(private trackerStore: Store<TrackerState>, private dashboardStore: Store<DashboardState>) { }
 
   getTimeVsDateGraphData(): Observable<GraphDataItem[]> {
+    this.dashboardStore.select(dashboardSelectors.dateList).subscribe(dateList => this.dateRange = dateList);
     const groupingsByDate = this.trackerStore.select(trackerSelectors.historyGroupingsByDate);
     const groupingsToGraph = groupingsByDate.map(grouping => filterGroupingsByDateRange(grouping, this.dateRange));
     return groupingsToGraph.map(groupings => {
@@ -29,12 +28,14 @@ export class GraphService {
   }
 
   getTimeVsPlatformGraphData(): Observable<GraphDataItem[]> {
+    this.dashboardStore.select(dashboardSelectors.dateList).subscribe(dateList => this.dateRange = dateList);
     const groupingsByPlatform = this.trackerStore.select(trackerSelectors.historyGroupingsByPlatform);
     const groupingsToGraph = groupingsByPlatform.map(grouping => filterGroupingsByDateRange(grouping, this.dateRange));
     return groupingsToGraph.map(groupings => mapToGraphData(groupings));
   }
 
   getTimeVsGameGraphData(): Observable<GraphDataItem[]> {
+    this.dashboardStore.select(dashboardSelectors.dateList).subscribe(dateList => this.dateRange = dateList);
     const groupingsByGame = this.trackerStore.select(trackerSelectors.historyGroupingsByGame);
     const groupingsToGraph = groupingsByGame.map(grouping => filterGroupingsByDateRange(grouping, this.dateRange));
     return groupingsToGraph.map(groupings => mapToGraphData(groupings));
