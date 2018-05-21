df500656b16caff7c853c0167f6234014f21c17d
Mike Zrimsek <mikezrimsek@gmail.com>
2018-05-21 14:40:27 -0400

    fix tests

--- src/app/features/library/library.component.spec.ts
+++ src/app/features/library/library.component.spec.ts
@@ -1,25 +1,45 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { combineReducers, Store, StoreModule } from '@ngrx/store';
+
 import { LibraryComponent } from './library.component';
+
+import * as fromRoot from '../../reducers/root.reducer';
+import * as fromShared from '../../shared/reducers/root.reducer';
 
 describe('LibraryComponent', () => {
   let component: LibraryComponent;
   let fixture: ComponentFixture<LibraryComponent>;
+  let store: Store<fromShared.State>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [LibraryComponent]
-    })
-      .compileComponents();
-  }));
+      declarations: [
+        LibraryComponent
+      ],
+      imports: [
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          'shared': combineReducers(fromShared.reducers)
+        })
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
+    }).compileComponents();
 
-  beforeEach(() => {
+    store = TestBed.get(Store);
+    spyOn(store, 'select').and.callThrough();
+
     fixture = TestBed.createComponent(LibraryComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('Should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Should select groupings by game', () => {
+    expect(store.select).toHaveBeenCalledWith(fromShared._selectHistoryGroupingsByGame);
   });
 });
