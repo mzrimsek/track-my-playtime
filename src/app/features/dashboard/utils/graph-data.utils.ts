a0a46b0c0763bf878c87c85f6094d417263b7bec
Mike Zrimsek <mikezrimsek@gmail.com>
2018-06-01 00:49:02 -0400

    add game graph data sorted

--- src/app/features/dashboard/utils/graph-data.utils.ts
+++ src/app/features/dashboard/utils/graph-data.utils.ts
@@ -4,7 +4,7 @@ import { HistoryGrouping } from '../../../shared/models';
 import { GraphDataItem } from '../models';
 
 import { filterGroupingsByDateRange } from '../../../shared/utils/history.utils';
-import { mapToGraphData, padDateGraphData } from './graph.utils';
+import { mapToGraphData, padDateGraphData, sortGraphDataByValue } from './graph.utils';
 
 export const getPaddedGraphData =
   (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
@@ -20,5 +20,14 @@ export const getGraphData =
     return groupings.combineLatest(dateRange, (groups, dates) => {
       const groupingsToGraph = filterGroupingsByDateRange(groups, dates);
       return mapToGraphData(groupingsToGraph);
+    });
+  };
+
+export const getSortedGraphData =
+  (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
+    return groupings.combineLatest(dateRange, (groups, dates) => {
+      const groupingsToGraph = filterGroupingsByDateRange(groups, dates);
+      const graphItems = mapToGraphData(groupingsToGraph);
+      return sortGraphDataByValue(graphItems);
     });
   };
