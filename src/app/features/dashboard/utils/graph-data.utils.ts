b2a1cf0fec71839084548337c829137f4c45f0a1
Mike Zrimsek <mikezrimsek@gmail.com>
2018-06-01 01:07:50 -0400

    refactor graph data utils

--- src/app/features/dashboard/utils/graph-data.utils.ts
+++ src/app/features/dashboard/utils/graph-data.utils.ts
@@ -6,28 +6,32 @@ import { GraphDataItem } from '../models';
 import { filterGroupingsByDateRange } from '../../../shared/utils/history.utils';
 import { mapToGraphData, padDateGraphData, sortGraphDataByValue } from './graph.utils';
 
+type GraphDataModifier = (items: GraphDataItem[], dates: Date[]) => GraphDataItem[];
+
 export const getPaddedGraphData =
   (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
-    return groupings.combineLatest(dateRange, (groups, dates) => {
-      const groupingsToGraph = filterGroupingsByDateRange(groups, dates);
-      const graphItems = mapToGraphData(groupingsToGraph).reverse();
-      return padDateGraphData(graphItems, dates);
-    });
+    const modifier: GraphDataModifier = (items: GraphDataItem[], dates: Date[]) => {
+      const reversed = items.reverse();
+      return padDateGraphData(reversed, dates);
+    };
+    return processGraphData(groupings, dateRange, modifier);
   };
 
 export const getGraphData =
   (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
-    return groupings.combineLatest(dateRange, (groups, dates) => {
-      const groupingsToGraph = filterGroupingsByDateRange(groups, dates);
-      return mapToGraphData(groupingsToGraph);
-    });
+    return processGraphData(groupings, dateRange, x => x);
   };
 
 export const getSortedGraphData =
   (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>): Observable<GraphDataItem[]> => {
+    return processGraphData(groupings, dateRange, sortGraphDataByValue);
+  };
+
+const processGraphData =
+  (groupings: Observable<HistoryGrouping[]>, dateRange: Observable<Date[]>, modifier: GraphDataModifier): Observable<GraphDataItem[]> => {
     return groupings.combineLatest(dateRange, (groups, dates) => {
       const groupingsToGraph = filterGroupingsByDateRange(groups, dates);
       const graphItems = mapToGraphData(groupingsToGraph);
-      return sortGraphDataByValue(graphItems);
+      return modifier(graphItems, dates);
     });
   };
