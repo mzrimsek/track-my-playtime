b042155ad88275c246f69cc7ef38112a72e7cf62
Mike Zrimsek <mikezrimsek@gmail.com>
2018-06-07 20:28:26 -0400

    add shared module to completion feature

--- src/app/features/completion/completion.module.ts
+++ src/app/features/completion/completion.module.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
+import { SharedModule } from '../../shared/shared.module';
+
 import { CompletionComponent } from './completion.component';
 import { CompletedComponent } from './components/completed/completed.component';
 import { PlayingComponent } from './components/playing/playing.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   declarations: [CompletionComponent, CompletedComponent, PlayingComponent]
 })
