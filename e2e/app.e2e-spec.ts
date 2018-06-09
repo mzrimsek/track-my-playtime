bfcd5ac7d9e7387d9265554e913d39c5742ab5b3
Mike Zrimsek <mikezrimsek@gmail.com>
2018-06-09 19:43:55 -0400

    fix e2e tests
    will need to add more of these in the future

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('track-my-playtime App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getWelcomeMessage()).toEqual('GAME TIME TRACKING MADE SIMPLE');
   });
 });
