0f910053d396c9c95074cc920f5306f0a9f32e69
Mike Zrimsek <mikezrimsek@gmail.com>
2018-07-12 10:24:42 -0400

    moving to not having redundant components

--- src/app/features/completion/components/completed/completed.component.spec.ts
+++ src/app/features/completion/components/completed/completed.component.spec.ts
@@ -23,86 +23,10 @@ describe('CompletedComponent', () => {
   }));
 
   describe('getDisplayData', () => {
-    it('Should return empty when no items', async(() => {
-      const result = component.getDisplayData();
-      expect(result.length).toBe(0);
-    }));
 
-    it('Should return empty when no item start entry match', async(() => {
-      component.items = [{
-        id: '1',
-        startEntryId: 'some start id',
-        endEntryId: 'some end id'
-      }];
-      component.gameGroupings = [
-        {
-          key: 'some game',
-          historyItems: [{
-            id: 'not some start id',
-            game: 'some game',
-            platform: 'some platform',
-            startTime: 1000,
-            endTime: 4000,
-            dateRange: [new Date(1000), new Date(4000)],
-            locked: false
-          }],
-          totalTime: 3
-        }
-      ];
-      fixture.detectChanges();
 
-      const result = component.getDisplayData();
 
-      expect(result.length).toBe(0);
-    }));
 
-    it('Should return correct data when item start entry matches', async(() => {
-      component.items = [{
-        id: '1',
-        startEntryId: 'some start id',
-        endEntryId: 'some end id'
-      }];
-      component.gameGroupings = [
-        {
-          key: 'some game',
-          historyItems: [{
-            id: 'some end id',
-            game: 'some game',
-            platform: 'some platform',
-            startTime: 5000,
-            endTime: 6000,
-            dateRange: [new Date(5000), new Date(6000)],
-            locked: false
-          }, {
-            id: 'some start id',
-            game: 'some game',
-            platform: 'some platform',
-            startTime: 1000,
-            endTime: 4000,
-            dateRange: [new Date(1000), new Date(4000)],
-            locked: false
-          }],
-          totalTime: 4
-        }
-      ];
-      fixture.detectChanges();
 
-      const result = component.getDisplayData();
-
-      expect(result).toEqual([{
-        item: {
-          id: '1',
-          startEntryId: 'some start id',
-          endEntryId: 'some end id'
-        },
-        completedItem: {
-          game: 'some game',
-          platform: 'some platform',
-          startTime: 1000,
-          endTime: 6000,
-          timePlayed: 4
-        }
-      }]);
-    }));
   });
 });
