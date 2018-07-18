0fb5f8041d8a9ebdabdf4c1f4bb9003cdd0c1d55
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-07-18 16:36:09 -0400

    add tabs tests

--- src/app/features/completion/components/tabs/tabs.component.spec.ts
+++ src/app/features/completion/components/tabs/tabs.component.spec.ts
@@ -1,15 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { combineReducers, Store, StoreModule } from '@ngrx/store';
+
 import { TabsComponent } from './tabs.component';
 
+import * as tabActions from '../../actions/tab.actions';
+
+import * as fromRoot from '../../../../reducers/root.reducer';
+import * as fromCompletion from '../../reducers/root.reducer';
+
 describe('TabsComponent', () => {
+  let store: Store<fromRoot.State>;
   let component: TabsComponent;
   let fixture: ComponentFixture<TabsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [TabsComponent]
+      declarations: [TabsComponent],
+      imports: [
+        StoreModule.forRoot({
+          ...fromRoot.reducers,
+          'completion': combineReducers(fromCompletion.reducers)
+        })
+      ]
     }).compileComponents();
+
+    store = TestBed.get(Store);
+
+    spyOn(store, 'dispatch').and.callThrough();
 
     fixture = TestBed.createComponent(TabsComponent);
     component = fixture.componentInstance;
@@ -18,5 +36,27 @@ describe('TabsComponent', () => {
 
   it('Should create the component', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('Playing Tab Clicked', () => {
+    it('Should dispatch SetVisibleTab with "Playing"', () => {
+      const playingTab = fixture.nativeElement.querySelector('.tabs .playing');
+      const action = new tabActions.SetVisibleTab('PLAYING');
+
+      playingTab.click();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
+  });
+
+  describe('Completed Tab Clicked', () => {
+    it('Should dispatch SetVisibleTab with "Completed"', () => {
+      const completedTab = fixture.nativeElement.querySelector('.tabs .completed');
+      const action = new tabActions.SetVisibleTab('COMPLETED');
+
+      completedTab.click();
+
+      expect(store.dispatch).toHaveBeenCalledWith(action);
+    });
   });
 });
