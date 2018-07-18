381d9c9a8005dd6e317ae082c35409c6714709d6
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-07-18 09:52:47 -0400

    styled completed item

--- src/app/features/completion/components/completed-item/completed-item.component.ts
+++ src/app/features/completion/components/completed-item/completed-item.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 
 import { UserService } from '../../../auth/services/user.service';
@@ -20,6 +21,9 @@ export class CompletedItemComponent implements OnInit {
 
   @Input() displayData: CompletedDisplayData;
   userId = '';
+  icons = {
+    remove: faTrash
+  };
   constructor(private store: Store<State>, private userService: UserService) { }
 
   ngOnInit() {
