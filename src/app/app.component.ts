42705ecdf48c2ea480ead8fe1f9a14fcfaaec3f7
Mike Zrimsek <mikezrimsek@gmail.com>
2018-07-18 23:00:05 -0400

    loading screen working
    need to do tests

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Store } from '@ngrx/store';
 
 import { insertAnalyticsElements } from 'insert-analytics-elements/googleTagManager';
+import { Observable } from 'rxjs/Observable';
 
 import * as actions from './actions/app.actions';
 
-import { State } from './reducers/root.reducer';
+import { State as RootState } from './reducers/root.reducer';
+import sharedSelectors, { State as SharedState } from './shared/reducers/root.reducer';
 
 import { environment } from '../environments/environment';
 
@@ -16,11 +18,16 @@ import { environment } from '../environments/environment';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
-  constructor(private store: Store<State>, private router: Router) {
-    this.store.dispatch(new actions.InitializeApplication());
+  userDataLoaded$: Observable<boolean>;
+  constructor(private rootStore: Store<RootState>, private sharedStore: Store<SharedState>, private router: Router) { }
+
+  ngOnInit() {
+    this.rootStore.dispatch(new actions.InitializeApplication());
     insertAnalyticsElements(environment.googleTagManager);
+
+    this.userDataLoaded$ = this.sharedStore.select(sharedSelectors.userDataLoaded);
   }
 
   shouldShowHeader(): boolean {
