a9bc1d2cb89983842eed714bbe7b39ed000b8906
Mike Zrimsek <mikezrimsek@gmail.com>
2018-08-14 22:37:09 -0400

    add reducer tests

---
+++ src/app/features/profile/reducers/profile.reducer.spec.ts
@@ -0,0 +1,30 @@
+import * as actions from '../actions/profile.actions';
+
+import { reducer, State } from './profile.reducer';
+
+import { Profile } from '../models';
+
+describe('Profile Reducer', () => {
+  it('Should clear profile info when ClearProfile is dispatched', () => {
+    const initialState: State = {
+      displayName: 'profile name'
+    };
+    const newState = reducer(initialState, new actions.ClearProfile());
+    expect(newState).toEqual({
+      displayName: ''
+    });
+  });
+
+  it('Should set profile info when LoadProfileSucceeded is dispatched', () => {
+    const initialState: State = {
+      displayName: ''
+    };
+    const profile: Profile = {
+      displayName: 'profile name'
+    };
+
+    const newState = reducer(initialState, new actions.LoadProfileSucceeded(profile));
+
+    expect(newState).toEqual(profile);
+  });
+});
