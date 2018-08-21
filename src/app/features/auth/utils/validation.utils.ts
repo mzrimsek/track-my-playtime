33f8f4a29b1b36ec8573bee3da4b948355f59350
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-08-21 10:12:12 -0400

    add more validation cases
    when you attempt to sign up with oauth and have an account it throws an error saying the account email address is already in use

--- src/app/features/auth/utils/validation.utils.ts
+++ src/app/features/auth/utils/validation.utils.ts
@@ -2,13 +2,22 @@ import * as userActions from '../actions/user.actions';
 
 import { Error } from '../../../models';
 
+export const EMAIL_IN_USE = 'Email address already in use.';
+export const INVALID_CREDENTIALS = 'Email or password invalid.';
+
 export const getValidationMessage = (error: Error): string => {
   switch (error.action) {
     case userActions.SIGNUP: {
-      return 'Email address already in use.';
+      return EMAIL_IN_USE;
     }
     case userActions.EMAIL_LOGIN: {
-      return 'Email or password invalid.';
+      return INVALID_CREDENTIALS;
+    }
+    case userActions.GOOGLE_LOGIN: {
+      return EMAIL_IN_USE;
+    }
+    case userActions.FACEBOOK_LOGIN: {
+      return EMAIL_IN_USE;
     }
     default: {
       return '';
