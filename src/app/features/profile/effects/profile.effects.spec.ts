a0ee7dfb5b90bd1a111b5ef985fa225545cbbe8e
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-09-06 10:10:46 -0400

    profile feature

--- src/app/features/profile/effects/profile.effects.spec.ts
+++ src/app/features/profile/effects/profile.effects.spec.ts
@@ -3,8 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { provideMockActions } from '@ngrx/effects/testing';
 
 import { cold, hot } from 'jasmine-marbles';
-import { Observable } from 'rxjs/Observable';
-import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Observable, ReplaySubject } from 'rxjs';
 
 import { ProfileEffects } from './profile.effects';
 
