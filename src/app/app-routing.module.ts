8e13ed093f9f42cd28f3da70a575fcbf72a3d56d
Zrimsek, Mike <mike.zrimsek@rovisys.com>
2018-09-10 09:36:24 -0400

    change quotes type?

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -48,7 +48,7 @@ const routes: Routes = [
       },
       {
         path: '**',
-        redirectTo: `/app/tracker`,
+        redirectTo: '/app/tracker',
         pathMatch: 'full'
       }
     ]
