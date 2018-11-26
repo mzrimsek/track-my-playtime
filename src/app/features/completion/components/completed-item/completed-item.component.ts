86a5b30f3a6edd888bbc4152053d47d65761674f
Mike Zrimsek <mikezrimsek@gmail.com>
2018-11-26 11:56:32 -0500

    edit notes working needs styling and testing

--- src/app/features/completion/components/completed-item/completed-item.component.ts
+++ src/app/features/completion/components/completed-item/completed-item.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faEdit, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Store } from '@ngrx/store';
 
 import { UserService } from '../../../auth/services/user.service';
@@ -23,6 +23,7 @@ export class CompletedItemComponent implements OnInit {
   userId = '';
   editNotes = false;
   icons = {
+    edit: faEdit,
     remove: faTrash
   };
   constructor(private store: Store<State>, private userService: UserService) { }
